ca69c8a274921ee1d14b5eef2737d336372d3d21
Nicolas Fischer <67649989+nicolasfischer1@users.noreply.github.com>
2020-10-26 10:22:59 -0300

    Atualização do cabeçalho com identificação e contexto do arquivo que contêm a resolução do desafio

--- Resolucao-desafio-EDP_AcampamentoFerias-1167.c
+++ Resolucao-desafio-EDP_AcampamentoFerias-1167.c
@@ -3,7 +3,7 @@
 /* Profas Daniela {Bagatini, Saccol}.
 /* Resolução do exercício 1167 - "Acampamento de férias" (do Uri Online Judge).
 /* Iniciado em: 23/10/2020.
-/* Última modificação em: 28/10/2020.
+/* Última modificação em: 26/10/2020.
 /* Programadores: Nicolas Fischer e Vinicius Molz.
 */
 
